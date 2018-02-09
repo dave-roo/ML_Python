3db0bba3cebd3424db769efb5b7909f29e6f9f2e
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-02-09 19:05:29 +0100

    adding the b component
    adding the b component

--- ML_tut_bestfitslope_v1.py
+++ ML_tut_bestfitslope_v1.py
@@ -1,14 +1,23 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
 xs = np.array([1,2,3,2,1], dtype=np.float64)
 ys = np.array([1,2,3,4,2], dtype=np.float64)
 
-def best_fit_slope(xs,ys):
-	m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs)*mean(xs)) - mean(xs*xs)))
-	return m
+def best_fit_slope_and_intercept(xs,ys):
+	m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / 
+	((mean(xs)*mean(xs)) - mean(xs*xs)))
+	
+	# b = y - mx
+	b = mean(ys) - m*mean(xs)
+	return m, b
 
-m = best_fit_slope(xs,ys)
+m, b = best_fit_slope_and_intercept(xs,ys)
 
-print(m)
+regression_line = [(m*x)+b for x in xs]
+
+plt.scatter(xs,ys)
+plt.plot(xs, regression_line)
+plt.show()
